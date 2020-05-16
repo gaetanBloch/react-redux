b36a4e7d45848e979ef7ab88755a4f54647c8081
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-16 14:00:40 +0200

    Added redux-thunk dependency

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
-import * as Actions from '../../store/actions/action';
+import * as actionCreators from '../../store/actions/action';
 
 class Counter extends Component {
   render() {
@@ -41,12 +41,12 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    onIncrementCounter: () => dispatch(Actions.increment()),
-    onDecrementCounter: () => dispatch(Actions.decrement()),
-    onAddCounter: () => dispatch(Actions.add(5)),
-    onSubtractCounter: () => dispatch(Actions.subtract(5)),
-    onStoreResult: (counter) => dispatch(Actions.storeResult(counter)),
-    onDeleteResult: (id) => dispatch(Actions.deleteResult(id)),
+    onIncrementCounter: () => dispatch(actionCreators.increment()),
+    onDecrementCounter: () => dispatch(actionCreators.decrement()),
+    onAddCounter: () => dispatch(actionCreators.add(5)),
+    onSubtractCounter: () => dispatch(actionCreators.subtract(5)),
+    onStoreResult: (counter) => dispatch(actionCreators.storeResult(counter)),
+    onDeleteResult: (id) => dispatch(actionCreators.deleteResult(id)),
   };
 }
 
